b95d9966395a9c0d52c4484aef7986195b9eafc0
fahri frayuda <frayudafahri@gmail.com>
2026-01-15 10:19:16 +0700

    hapus produk

---
+++ pages/hapusproduk.php
@@ -0,0 +1,13 @@
+<?php
+// Baris ini menghubungkan file dengan database 
+include 'koneksi.php';
+
+// Mengambil ID barang yang akan dihapus dari URL 
+$id = $_GET['id'];
+
+// Menjalankan perintah SQL untuk menghapus data berdasarkan ID [cite: 8, 92]
+mysqli_query($conn, "DELETE FROM barang WHERE id_barang='$id'");
+
+// Mengalihkan kembali halaman ke daftar produk setelah berhasil menghapus [cite: 58, 89]
+header("Location: dashboard.php?page=listproducts");
+?>
